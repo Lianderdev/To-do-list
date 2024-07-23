be0b569db47db403aa16ae7b4905e88363ec17f0
Liander Souza <liandervinicius13@gmail.com>
2024-07-23 10:57:33 -0300

    style and functionality

--- script.js
+++ script.js
@@ -1,65 +1,74 @@
-const lista = document.querySelector('.tasks')
-const add = document.querySelector('.button-add')
+const lista = document.querySelector('.tasks');
+const add = document.querySelector('.button-add');
 
-add.addEventListener('click', () => {
-    const tarefa = document.querySelector('.input')
+add.addEventListener('click', createTask);
 
-    if (tarefa.value.trim() === '') {
-        alert('escreva algo')
-    } else {
-        // Criando o item da lista
-        const item = document.createElement('div')
-        item.classList.add('task')
-        lista.appendChild(item)
+// Adiciona o event listener para o campo de entrada
+const inputField = document.querySelector('.input');
+inputField.addEventListener('keydown', function(event) {
+  if (event.key === 'Enter') {
+    createTask();
+  }
+});
 
-        // Adicionando o checked e a tarefa
-        const task_info = document.createElement('div')
-        task_info.classList.add('task-info')
-        item.appendChild(task_info)
+function createTask() {
+  const tarefa = document.querySelector('.input');
 
-        const button_checked = document.createElement('button')
-        const icone_checked = document.createElement('i')
-        icone_checked.classList.add('fa-solid', 'fa-check')
-        button_checked.appendChild(icone_checked )
-        button_checked.classList.add('checked')
-        task_info.appendChild(button_checked)
+  if (tarefa.value.trim() === '') {
+    alert('Escreva algo');
+  } else {
+    // Criando o item da lista
+    const item = document.createElement('div');
+    item.classList.add('task');
+    lista.appendChild(item);
 
-        const p = document.createElement('p')
-        p.classList.add('task-paragraph')
-        p.textContent = tarefa.value
-        task_info.appendChild(p)
-        tarefa.value = ''
+    // Adicionando o checked e a tarefa
+    const task_info = document.createElement('div');
+    task_info.classList.add('task-info');
+    item.appendChild(task_info);
 
-        // adicinando os butões de configurações 
-        const buttons = document.createElement('div')
-        const button_editar = document.createElement('button')
-        const button_apagar = document.createElement('button')
-        const icone_apagar = document.createElement('i')
-        const icone_editar = document.createElement('i')
+    const button_checked = document.createElement('button');
+    const icone_checked = document.createElement('i');
+    icone_checked.classList.add('fa-solid', 'fa-check');
+    button_checked.appendChild(icone_checked);
+    button_checked.classList.add('checked');
+    task_info.appendChild(button_checked);
 
-        icone_apagar.classList.add('fa-solid', 'fa-x')
-        icone_editar.classList.add('fa-solid', 'fa-pencil')
+    const p = document.createElement('p');
+    p.classList.add('task-paragraph');
+    p.textContent = tarefa.value;
+    task_info.appendChild(p);
+    tarefa.value = '';
 
-        button_apagar.appendChild(icone_apagar)
-        button_editar.appendChild(icone_editar)
+    // Adicionando os botões de configurações 
+    const buttons = document.createElement('div');
+    const button_editar = document.createElement('button');
+    const button_apagar = document.createElement('button');
+    const icone_apagar = document.createElement('i');
+    const icone_editar = document.createElement('i');
 
-        buttons.classList.add('buttons')
-        button_apagar.classList.add('apagar')
-        button_editar.classList.add('editar')
+    icone_apagar.classList.add('fa-solid', 'fa-x');
+    icone_editar.classList.add('fa-solid', 'fa-pencil');
 
-        buttons.appendChild(button_editar)
-        buttons.appendChild(button_apagar)
+    button_apagar.appendChild(icone_apagar);
+    button_editar.appendChild(icone_editar);
 
-        item.appendChild(buttons)
+    buttons.classList.add('buttons');
+    button_apagar.classList.add('apagar');
+    button_editar.classList.add('editar');
 
-        // configarando as funções dos botões 
-        button_checked.addEventListener('click', () => {
-            p.classList.add('paragraph-checked')
-        })
+    buttons.appendChild(button_editar);
+    buttons.appendChild(button_apagar);
 
-        button_apagar.addEventListener('click', () => {
-            item.remove()
-        })
+    item.appendChild(buttons);
+
+    // Configurando as funções dos botões 
+    button_checked.addEventListener('click', () => {
+      p.classList.toggle('paragraph-checked');
+    });
+
+    button_apagar.addEventListener('click', () => {
+      item.remove();
+    });
+  }
 }
-})
-
